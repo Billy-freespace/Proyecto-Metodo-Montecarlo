e2e8659b0ffbe0bdbfe1a3d7c4955042074a3ec5
datanicolas1 <nrojasm@uni.pe>
2018-06-21 17:41:32 -0500

    añadi fuerza bruta

---
+++ Proyecto-Montecarlo/FuerzaB.R
@@ -0,0 +1,29 @@
+#exponencial FUERZA BRUTA
+#Definimos la curva
+curve(exp,0.5,3.5,lwd=2,axes = F,xlab='',ylab = '')
+#Estableciendo el eje x
+axis(1)
+#Establecemos los parametros
+t <-seq(1,3,by = 0.01)
+x <- c(1,t,3)
+y <- c(0,exp(t),0)
+#Creamos un contador en 0 quien llegara a ser la cantidad de puntos aleatorios
+contador <- 0
+#Definimos el numero de puntos aleatorios
+n = 1000
+#Definimos un bucle for que ira distinguiendo el numero de puntos bajo la grafica
+for (i in 1:n) {
+  x <-runif(n,1,3)
+  y <-runif(n,exp(1),exp(3))
+  if(y[i] <= exp(x[i]))
+    contador <- contador +1
+}
+#Calculamos la probabilidad
+prob <-(contador/n)
+prob
+
+#Calculapmos el area bajo la curva aprox
+area <- prob*2*exp(3)
+area
+#Comparamos con la verdadera area
+exp(3)-exp(1)
